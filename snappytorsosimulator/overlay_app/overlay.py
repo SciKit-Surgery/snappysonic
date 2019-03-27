a14272475720268c453be686cebd5f94cdf54006
s.thompson@ucl.ac.uk <s.thompson@ucl.ac.uk>
2019-03-27 17:11:04 +0000

    Issue #1 implementing

--- snappytorsosimulator/overlay_app/overlay.py
+++ snappytorsosimulator/overlay_app/overlay.py
@@ -5,21 +5,13 @@
 from itertools import cycle
 from sys import version_info, exit
 from vtk.util import numpy_support
-import vtk
+import cv2
+import numpy
 from PySide2.QtWidgets import QApplication
 from sksurgeryutils.common_overlay_apps import OverlayBaseApp
 from sksurgerynditracker.nditracker import NDITracker
 from sksurgeryarucotracker.arucotracker import ArUcoTracker
 from sksurgerytrackervisualisation.shapes.cylinder import VTKCylinderModel
-
-def np2vtk(mat):
-    if mat.shape == (4, 4):
-        obj = vtk.vtkMatrix4x4()
-        for i in range(4):
-            for j in range(4):
-                obj.SetElement(i, j, mat[i, j])
-        return obj
-
 
 def configure_tracker (config):
     if "tracker type" not in config:
@@ -48,10 +40,10 @@ class OverlayApp(OverlayBaseApp):
         if "ultrasound buffer" in config:
             #and call the constructor for the base class
             if version_info > (3, 0):
-                super().__init__(config.get("image source"))
+                super().__init__(config.get("ultrasound buffer"))
             else:
                 #super doesn't work the same in py2.7
-                OverlayBaseApp.__init__(self, config.get("image source"))
+                OverlayBaseApp.__init__(self, config.get("ultrasound buffer"))
         else:
             raise KeyError ("Configuration must contain an ultrasound buffer")
 
@@ -62,52 +54,44 @@ class OverlayApp(OverlayBaseApp):
         frame_counter = 0
         
         if "buffer descriptions" in config:
-            for usbuffer in congig.get("buffer descriptions"):
-                start_frame = config.get("start frame")
+            for usbuffer in config.get("buffer descriptions"):
+                start_frame = usbuffer.get("start frame")
+                end_frame = usbuffer.get("end frame")
                 tempbuffer = []
                 if start_frame == frame_counter:
-                    ret, image = self.video_source.read()
-                    frame_counter = frame_counter + 1
-
-
-
-        for 
-        if "loop video" in config:
-            if config.get("loop video"):
-                while ret:
-                    video_buffer.append(image)
-                    ret, image = self.video_source.read()
-
-                self._video_loop_buffer = cycle(video_buffer)
+                    while frame_counter < end_frame:
+                        ret, image = self.video_source.read()
+                        frame_counter = frame_counter + 1
+                        tempbuffer.append(image)
+                print ("adding frame ", len(tempbuffer), " images to buffer ", usbuffer.get("name"))
+                usbuffer.update({"buffer" : tempbuffer})
+                
+                self._video_buffers.append(usbuffer)
+                
 
         self._tracker = None
+        
         if "tracker config" in config:
             tracker_config = config.get("tracker config")
             self._tracker = configure_tracker(config.get("tracker config"))
-
-
-
-        cylinder = VTKCylinderModel(10.0, 5.0, (1.0, 0.0, 0.0), 'cyl01',True,1.0)
-        models=[]
-        models.append(cylinder)
-        self.vtk_overlay_window.add_vtk_models(models)
+        
+        #this is a bit of a hack. Is there a better way?
+        bgimage = self._tracker._capture.read()
+        print (numpy.shape(bgimage))
+        height, width, channels = numpy.shape(bgimage)
+        self._backgroundimage = numpy.zeros((height, width, channels), numpy.uint8)
 
     def update(self):
         """Update the background renderer with a new frame,
         move the model and render"""
-        if  self._video_loop_buffer:
-            image = next (self._video_loop_buffer)
-        else:
-            _, image = self.video_source.read()
-
         #add a method to move the rendered models
-        self._update_tracking()
+        image = self._get_image_with_tracking()
 
         self.vtk_overlay_window.set_video_image(image)
         self.vtk_overlay_window.Render()
         #self.vtk_overlay_window._RenderWindow.Render()
 
-    def _update_tracking(self):
+    def _get_image_with_tracking(self):
         """Internal method to move the rendered models in
         some interesting way
         #Iterate through the rendered models
@@ -124,28 +108,46 @@ class OverlayApp(OverlayBaseApp):
 
         if port_handles:
             #these will need working on, need a way to match model names with port handles
-            for trk in tracking:
-                for actor in self.vtk_overlay_window.get_foreground_renderer().GetActors():
-                    print (trk)
-                    actor.SetUserMatrix(np2vtk(trk))
-        self.vtk_overlay_window.set_camera_state({"ClippingRange": [10, 800]})
+            pass
+        
+        usbuffer = self._video_buffers[0].get("buffer")
+        image = usbuffer[0]
+        cv2.imshow('tracking',self._backgroundimage)
+        return image
 
 #here's a dummy app just to test the class. Quickly
 if __name__ == '__main__':
     app = QApplication([])
 
-    configuration = { "image source" : "../../data/noisy_logo.avi",
-                      "loop video"   : True,
+    configuration = { "ultrasound buffer" : "../../data/glove2.mp4",
+                      "buffer descriptions" : (
+                                               { "name" : "glove",
+                                                 "start frame" : 0,
+                                                 "end frame" : 100,
+                                                 "x0" : 80 , "x1" : 160,
+                                                 "y0" : 20 , "y1" : 200 },
+                                               { "name" : "glove2",
+                                                 "start frame" : 101,
+                                                 "end frame" : 200,
+                                                 "x0" : 80 , "x1" : 160,
+                                                 "y0" : 20 , "y1" : 200 }
+                                               ),
+
                         "tracker config" :
                         {
                             "tracker type" : "aruco",
-                            "debug" : True
+                            "video source" : 2,
+                            "debug" : True,
+                            "capture properties" : 
+                            {
+                                "CAP_PROP_FRAME_WIDTH" : 1280 ,
+                                "CAP_PROP_FRAME_HEIGHT" : 960 
+                            }
+
                         }
 
                     }
 
-    configuration_live = { "image source" : 0,
-                      "loop video"   : False }
     viewer = OverlayApp(configuration)
 
     #model_dir = '../models'
