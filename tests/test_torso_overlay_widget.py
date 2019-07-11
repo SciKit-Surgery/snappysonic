8033b579cefc427f576b3402a7d82850c887c678
s.thompson@ucl.ac.uk <s.thompson@ucl.ac.uk>
2019-07-11 13:58:04 +0100

    Issue #4 removed verbosity from testing

--- tests/test_torso_overlay_widget.py
+++ tests/test_torso_overlay_widget.py
@@ -2,7 +2,6 @@
 
 """snappy-torso-simulator tests"""
 
-from time import time
 import pytest
 from sksurgeryutils.common_overlay_apps import OverlayBaseApp
 from sksurgerytorsosimulator.overlay_widget.overlay import OverlayApp
@@ -13,28 +12,14 @@ def test_overlay_app(setup_qt):
     but doing this seems to avoid time out errors on the mac
     CI server
     """
-    print("Starting test: ", time())
     _ = setup_qt
     _ = OverlayBaseApp('data/aruco_tag.avi')
-    print("Ending test: ", time())
-
-
-def test_overlay_app_with_mp4(setup_qt):
-    """Test that OverlayBaseApp is working, I'm not sure why
-    but doing this seems to avoid time out errors on the mac
-    CI server
-    """
-    print("Starting test: ", time())
-    _ = setup_qt
-    _ = OverlayBaseApp('data/usbuffer.mp4')
-    print("Ending test: ", time())
 
 
 def test_error_on_ultrasound_buffer(setup_qt):
     """
     Test we get a key error if no usbuffer
     """
-    print("Starting test: ", time())
     _ = setup_qt
 
     config = {
@@ -56,13 +41,12 @@ def test_error_on_ultrasound_buffer(setup_qt):
     }
     with pytest.raises(KeyError):
         _ = OverlayApp(config)
-    print("Ending test: ", time())
+
 
 def test_error_on_invalid_buffer(setup_qt):
     """
     Test we get a value error if we can't read the us buffer
     """
-    print("Starting test: ", time())
     _app = setup_qt
     config = {
         "default image": "data/logo.png",
@@ -85,14 +69,12 @@ def test_error_on_invalid_buffer(setup_qt):
     config.update({"ultrasound buffer": "data/aruco_tag.avi"})
     with pytest.raises(ValueError):
         _ = OverlayApp(config)
-    print("Ending test: ", time())
 
 
 def test_init_no_logo(setup_qt):
     """
     Test we can initialise widget, and run with default image set
     """
-    print("Starting test: ", time())
     _ = setup_qt
     config = {
         "default image": "data/logo.png",
@@ -117,7 +99,6 @@ def test_init_no_logo(setup_qt):
     overlay_widget = OverlayApp(config)
     overlay_widget.update()
     overlay_widget.stop()
-    print("Ending test: ", time())
 
 
 def test_and_run_with_logo(setup_qt):
@@ -125,7 +106,6 @@ def test_and_run_with_logo(setup_qt):
     Test we can initialise widget and run update,
     when we haven't set default image.
     """
-    print("Starting test: ", time())
     _ = setup_qt
     config = {
         "ultrasound buffer": "data/usbuffer.mp4",
@@ -148,7 +128,6 @@ def test_and_run_with_logo(setup_qt):
     overlay_widget = OverlayApp(config)
     overlay_widget.update()
     overlay_widget.stop()
-    print("Ending test: ", time())
 
 
 def test_and_run_with_buffer_data(setup_qt):
@@ -156,7 +135,6 @@ def test_and_run_with_buffer_data(setup_qt):
     Test we can initialise widget and run update,
     getting an image from the usbuffer
     """
-    print("Starting test: ", time())
     _ = setup_qt
     config = {
         "ultrasound buffer": "data/usbuffer.mp4",
@@ -179,4 +157,3 @@ def test_and_run_with_buffer_data(setup_qt):
     overlay_widget = OverlayApp(config)
     overlay_widget.update()
     overlay_widget.stop()
-    print("Ending test: ", time())
