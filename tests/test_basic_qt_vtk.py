d77df4bd4c19e475154ff39d5d671708d192c9d8
Thomas Dowrick <tdowrick@gmail.com>
2020-06-11 16:31:34 +0100

    Run different overlay apps to find where problem originates

--- tests/test_basic_qt_vtk.py
+++ tests/test_basic_qt_vtk.py
@@ -2,6 +2,8 @@ import pytest
 import PySide2
 
 import sys
+from sksurgeryutils.common_overlay_apps import OverlayBaseApp
+from snappysonic.overlay_widget.overlay import OverlayApp
 
 from PySide2.QtWidgets import QApplication, QDialog, QLineEdit, QPushButton
 from sksurgeryvtk.widgets.vtk_overlay_window import VTKOverlayWindow
@@ -12,83 +14,39 @@ from sksurgeryvtk.widgets.QVTKRenderWindowInteractor \
 import vtk
 from vtk.util.colors import tomato
 
-@pytest.fixture(scope="session")
-def setup_qt():
-
-    """ Create the QT application. """
-    app = QApplication([])
-    return app
-
-def test_qapp(setup_qt):
-
-    app = setup_qt
-    dialog = QDialog()
-    dialog.show()
-
-def test_vtk_cylinder(setup_qt):
+def test_overlay_app(setup_qt):
+    _ = setup_qt
     
-    #Copied from https://vtk.org/Wiki/VTK/Examples/Python/Cylinder
+    config = {
+        "ultrasound buffer": "data/usbuffer.mp4",
+        "buffer descriptions": [
+            {
+                "name": "xxxx",
+                "start frame": 0,
+                "end frame": 284,
+                "x0": 40, "x1": 240,
+                "y0": 200, "y1": 260,
+                "scan direction": "x"
+            }],
+        "tracker config": {
+            "tracker type": "aruco",
+            "video source": "data/aruco_tag.avi",
+            "debug": False,
+        }
+    }
 
-    # This creates a polygonal cylinder model with eight circumferential
-    # facets.
-    cylinder = vtk.vtkCylinderSource()
-    cylinder.SetResolution(8)
+    # This widget inherits from OverlayBaseApp
+    overlay_widget = OverlayApp(config)    
+    overlay_widget.update()
+    overlay_widget.stop()
 
-    # The mapper is responsible for pushing the geometry into the graphics
-    # library. It may also do color mapping, if scalars or other
-    # attributes are defined.
-    cylinderMapper = vtk.vtkPolyDataMapper()
-    cylinderMapper.SetInputConnection(cylinder.GetOutputPort())
+def test_overlay_base_app(setup_qt):
+    _ = setup_qt
 
-    # The actor is a grouping mechanism: besides the geometry (mapper), it
-    # also has a property, transformation matrix, and/or texture map.
-    # Here we set its color and rotate it -22.5 degrees.
-    cylinderActor = vtk.vtkActor()
-    cylinderActor.SetMapper(cylinderMapper)
-    cylinderActor.GetProperty().SetColor(tomato)
-    cylinderActor.RotateX(30.0)
-    cylinderActor.RotateY(-45.0)
+    # This includesa  VTKOverlayWindow()
+    overlay_base_app = OverlayBaseApp("data/usbuffer.mp4")
 
-    # Create the graphics structure. The renderer renders into the render
-    # window. The render window interactor captures mouse events and will
-    # perform appropriate camera or actor manipulation depending on the
-    # nature of the events.
-    ren = vtk.vtkRenderer()
-    renWin = vtk.vtkRenderWindow()
-    renWin.AddRenderer(ren)
-    iren = vtk.vtkRenderWindowInteractor()
-    iren.SetRenderWindow(renWin)
+def test_vtk_overlay_widget(setup_qt):
+    _ = setup_qt
 
-    # Add the actors to the renderer, set the background and size
-    ren.AddActor(cylinderActor)
-    ren.SetBackground(0.1, 0.2, 0.4)
-    renWin.SetSize(200, 200)
-
-    # This allows the interactor to initalize itself. It has to be
-    # called before an event loop.
-    iren.Initialize()
-
-    # We'll zoom in a little by accessing the camera and invoking a "Zoom"
-    # method on it.
-    ren.ResetCamera()
-    ren.GetActiveCamera().Zoom(1.5)
-    renWin.Render()
-
-    # Start the event loop.
-    #iren.Start()
-
-# This should pass
-def test_qvtkrenderwindowinteracto_dont_start(setup_qt):
-    app = setup_qt
-    widget = QVTKRenderWindowInteractor()
-
-# This should fail/crash
-def test_qvtkrenderwindowinteractor(setup_qt):
-    app = setup_qt
-    widget = QVTKRenderWindowInteractor()
-    widget.Start()
-
-def test_vtkoverlaywindow(setup_qt):
-    app = setup_qt
-    widget = VTKOverlayWindow()
-
+    overlay_widget = VTKOverlayWindow()
