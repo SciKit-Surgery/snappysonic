c089924f16473ab04eaf9c30a2a2cb3eddfaedfe
Stephen Thompson <s.thompson@ucl.ac.uk>
2023-03-28 10:24:52 +0100

    Issue #23 added no_init to overlay window

--- snappysonic/overlay_widget/overlay.py
+++ snappysonic/overlay_widget/overlay.py
@@ -33,7 +33,7 @@ class OverlayApp(OverlayBaseWidget):
 
         if "ultrasound buffer" in config:
             #and call the constructor for the base class
-            super().__init__(config.get("ultrasound buffer"))
+            super().__init__(config.get("ultrasound buffer"), init_vtk_widget=False)
         else:
             raise KeyError("Configuration must contain an ultrasound buffer")
 
